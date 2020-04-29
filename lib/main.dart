06ea616b7ceb8793e8bc77b888ba1eab02312130
Mantab Tadayon <mt.tadayon@gmail.com>
2020-04-29 11:15:33 +0200

    Complete the front container

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() => runApp(MyApp());
 
@@ -25,45 +26,64 @@ class MyHomePage extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 80.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+            child: Stack(
               children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(40.0),
-                  decoration: BoxDecoration(color: Colors.pinkAccent),
-                  child: Column(
-                    children: <Widget>[
-                      Icon(Icons.build),
-                      Text(
-                        'City break',
-                        style: TextStyle(
-                            fontSize: 32.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(40.0),
+                      decoration: BoxDecoration(color: Color(0xffff62b2)),
+                      child: Column(
+                        children: <Widget>[
+                          Icon(
+                            Icons.build,
+                            color: Colors.white,
+                            size: 32.0,
+                          ),
+                          Text(
+                            'City break',
+                            style: TextStyle(
+                                fontSize: 32.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            'From €29',
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
                       ),
-                      Text(
-                        'From €29',
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 20.0),
+                      color: Colors.white,
+                      child: Text(
+                        'View me',
+                        textAlign: TextAlign.center,
                         style: TextStyle(
-                          fontSize: 24.0,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffff62b2)),
+                      ),
+                    )
+                  ],
+                ),
+                Align(
+                  alignment: Alignment(0, 0.1),
+                  child: Transform.rotate(
+                    angle: math.pi / 4,
+                    child: Container(
+                      height: 40.0,
+                      width: 40.0,
+                      color: Color(0xffff62b2),
+                    ),
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
-                  color: Colors.white,
-                  child: Text(
-                    'View me',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.pinkAccent),
-                  ),
-                )
               ],
             ),
           ),
