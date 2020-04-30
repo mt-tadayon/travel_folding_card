ce3c2bd8f97409de72bc76f17ba036b441e0f90d
Mantab Tadayon <mt.tadayon@gmail.com>
2020-04-30 17:09:40 +0200

    Create animation for the button and add flutter desktop to the project

---
+++ android/app/src/main/kotlin/com/mimifi/flutter_challenge_02/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mimifi.flutter_challenge_02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -48,21 +48,16 @@ class MyHomePage extends StatelessWidget {
                           style: TextStyle(
                             fontSize: 16.0,
                           )),
-                      SizedBox(height: 10.0,),
-                      FlatButton(
-                        onPressed: () {
-                          print('clicked');
-                        },
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                              color: Color(0xffff62b2),
-                              width: 2.0,
-                            ),
-                            borderRadius: BorderRadius.circular(5.0)),
-                        textColor: Color(0xffff62b2),
-                        focusColor: Colors.lightBlueAccent,
-                        child: Text(
-                          'View deals',
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Container(
+                        height: 40.0,
+                        child: AnimatedButton(
+                          width: 120.0,
+                          height: 120,
+                          color: Color(0xffff62b2),
+                          buttonText: 'View details',
                         ),
                       )
                     ],
@@ -130,5 +125,100 @@ class MyHomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class AnimatedButton extends StatefulWidget {
+  final double height;
+  final double width;
+  final String buttonText;
+  final Color color;
+
+  AnimatedButton({this.height, this.width, this.buttonText, this.color});
+
+  @override
+  _AnimatedButtonState createState() => _AnimatedButtonState();
+}
+
+class _AnimatedButtonState extends State<AnimatedButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+  Color textColor;
+
+  @override
+  void initState() {
+    super.initState();
+    textColor = widget.color;
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    animation = Tween(begin: 0.0, end: widget.height).animate(
+      CurvedAnimation(
+        curve: Curves.easeIn,
+        parent: controller,
+      ),
+    )..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: widget.height,
+        width: widget.width,
+        child: Material(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            side: BorderSide(width: 3.0, color: widget.color),
+          ),
+          child: InkWell(
+              onHover: (value) {
+                if (value) {
+                  controller.forward();
+                  setState(() {
+                    textColor = Colors.white;
+                  });
+                } else {
+                  controller.reverse();
+                  setState(() {
+                    textColor = widget.color;
+                  });
+                }
+              },
+              onTap: () {
+                print('Hello');
+              },
+              child: Stack(
+                children: <Widget>[
+                  Center(
+                    child: Container(
+                      height: animation.value,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.pink,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: AnimatedDefaultTextStyle(
+                      child: Text(widget.buttonText),
+                      duration: Duration(milliseconds: 200),
+                      style: TextStyle(
+                        color: textColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      curve: Curves.easeIn,
+                    ),
+                  )
+                ],
+              )),
+        ));
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
